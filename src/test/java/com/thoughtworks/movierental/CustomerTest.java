0f352d7772de5abb9692287b46ed773197bf1729
Sagar Bhatt <sagarkbhatt95@gmail.com>
2019-04-03 22:51:51 +0530

    Update test case

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -27,24 +27,29 @@ public class CustomerTest {
         Rental rent;
         Movie mov;
 
-        mov = new Movie( "3 Idiots", 0 ); // regular
+        mov = new Movie( "MI4", 0 ); // REGULAR
+        rent = new Rental( mov , 2 );
+        sagar.addRental( rent );
+
+        mov = new Movie( "3 Idiots", 0 ); // REGULAR
         rent = new Rental( mov , 4 );
         sagar.addRental( rent );
 
-        mov = new Movie( "ABCD", 2 ); // children
+        mov = new Movie( "ABCD", 2 ); // CHILDRENS
         rent = new Rental( mov, 1 );
         sagar.addRental( rent );
 
-        mov = new Movie( "Gully Boy", 1 ); // new release
+        mov = new Movie( "Gully Boy", 1 ); // NEW_RELEASE
         rent = new Rental( mov, 4 );
         sagar.addRental( rent );
 
         String expected = "Rental Record for Sagar\n";
+        expected += "\tMI4\t2.0\n";
         expected += "\t3 Idiots\t5.0\n";
         expected += "\tABCD\t1.5\n";
         expected += "\tGully Boy\t12.0\n";
-        expected += "Amount owed is 18.5\n";
-        expected += "You earned 4 frequent renter points";
+        expected += "Amount owed is 20.5\n";
+        expected += "You earned 5 frequent renter points";
 
         dummyStatement =  expected;
         dummyData = sagar;
