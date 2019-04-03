ac15916b9fb0b1c12436e5c1d21fb86f802dbf5d
Sagar <in-sagar.bhatt@Sagars-MacBook-Pro.local>
2019-04-03 22:36:51 +0530

    Update test case

--- src/test/java/com/thoughtworks/movierental/CustomerTest.java
+++ src/test/java/com/thoughtworks/movierental/CustomerTest.java
@@ -1,9 +1,52 @@
 package com.thoughtworks.movierental;
 
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CustomerTest {
+
+    private Customer dummyData;
+
+    private String dummyStatement;
+
     @Test
     public void test(){}
 
+    @Test
+    public void testStatement() {
+
+        setCustomerDummyData();
+        assertEquals( dummyStatement, dummyData.statement() );
+    }
+
+    public void setCustomerDummyData() {
+
+        Customer sagar = new Customer( "Sagar" );
+
+        Rental rent;
+        Movie mov;
+
+        mov = new Movie( "3 Idiots", 0 ); // regular,5
+        rent = new Rental( mov , 4 );
+        sagar.addRental( rent );
+
+        mov = new Movie( "ABCD", 2 ); // children, 1.5
+        rent = new Rental( mov, 1 );
+        sagar.addRental( rent );
+
+        mov = new Movie( "Gully Boy", 1 ); // new release, 12
+        rent = new Rental( mov, 4 );
+        sagar.addRental( rent );
+
+        String expected = "Rental Record for Sagar\n";
+        expected += "\t3 Idiots\t5.0\n";
+        expected += "\tABCD\t1.5\n";
+        expected += "\tGully Boy\t12.0\n";
+        expected += "Amount owed is 18.5\n";
+        expected += "You earned 4 frequent renter points";
+
+        dummyStatement =  expected;
+        dummyData = sagar;
+    }
 }
